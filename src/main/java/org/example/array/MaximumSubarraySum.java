df2d5a252348ea4ca708e6547f5d4962c462c4a9
anita <talakhadzeani@gmail.com>
2024-01-17 00:17:42 +0200

    Update the docs and add a method for printing

--- src/main/java/org/example/array/MaximumSubarraySum.java
+++ src/main/java/org/example/array/MaximumSubarraySum.java
@@ -3,23 +3,26 @@ package org.example.array;
 public class MaximumSubarraySum {
 
     public static void main(String[] args) {
-        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 }; // 6
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4}; // 6
         assert maxSubArray(arr) == 6;
+
+        printMaxSubArray(arr);
     }
 
 
-    // Kadane's algorithm: the idea is to maintain a local maximum sum at each index
-    // and a global maximum sum so far. The local maximum sum at each index is the
-    // maximum of the current element and the sum of the current element and the
-    // local maximum sum at the previous index. The global maximum sum so far is the
-    // maximum of the local maximum sum at each index and the global maximum sum so
-    // far. The local maximum sum at each index is reset to 0 if it becomes
-    // negative because we don't want to include negative numbers in the sum as it
-    // will decrease the sum.
-    // Time complexity: O(n)
+    /**
+     * Kadane's algorithm:
+     * The idea of Kadane’s algorithm is to maintain a variable max_ending_here
+     * that stores the maximum sum contiguous subarray ending at current index
+     * and a variable max_so_far stores the maximum sum of contiguous subarray found so far.
+     * Everytime there is a positive-sum value in max_ending_here compare it with max_so_far
+     * and update max_so_far if it is greater than max_so_far.
+     * Time complexity: O(n)
+     */
     private static int maxSubArray(int[] arr) {
         int maxSoFar = arr[0];
         int maxEndingHere = Math.max(arr[0], 0);
+
         for (int i = 1; i < arr.length; i++) {
             maxEndingHere += arr[i];
             maxSoFar = Math.max(maxSoFar, maxEndingHere);
@@ -29,4 +32,29 @@ public class MaximumSubarraySum {
         return maxSoFar;
     }
 
+
+    private static void printMaxSubArray(int[] arr) {
+        int maxSoFar = arr[0];
+        int maxEndingHere = Math.max(arr[0], 0);
+        int start = 0;
+        int end = 0;
+        int s = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere += arr[i];
+            if (maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+                start = s;
+                end = i;
+            }
+            maxEndingHere = Math.max(maxEndingHere, 0);
+            if (maxEndingHere == 0) {
+                s = i + 1;
+            }
+        }
+
+        System.out.println("Maximum contiguous sum is " + maxSoFar);
+        System.out.println("Starting index " + start);
+        System.out.println("Ending index " + end);
+    }
 }
