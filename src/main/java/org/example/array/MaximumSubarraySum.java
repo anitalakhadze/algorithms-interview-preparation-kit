0b8b6fc572348be3720a1c42b3e6dddf8710516b
anita <talakhadzeani@gmail.com>
2024-01-17 00:10:05 +0200

    Add explanation behind the solution

--- src/main/java/org/example/array/MaximumSubarraySum.java
+++ src/main/java/org/example/array/MaximumSubarraySum.java
@@ -4,13 +4,20 @@ public class MaximumSubarraySum {
 
     public static void main(String[] args) {
         int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 }; // 6
-        System.out.println(maxSubArray(arr));
         assert maxSubArray(arr) == 6;
     }
 
-    private static int maxSubArray(int[] arr) {
-        // TODO: 1/16/24 write Kadane's algorithm
 
+    // Kadane's algorithm: the idea is to maintain a local maximum sum at each index
+    // and a global maximum sum so far. The local maximum sum at each index is the
+    // maximum of the current element and the sum of the current element and the
+    // local maximum sum at the previous index. The global maximum sum so far is the
+    // maximum of the local maximum sum at each index and the global maximum sum so
+    // far. The local maximum sum at each index is reset to 0 if it becomes
+    // negative because we don't want to include negative numbers in the sum as it
+    // will decrease the sum.
+    // Time complexity: O(n)
+    private static int maxSubArray(int[] arr) {
         int maxSoFar = arr[0];
         int maxEndingHere = Math.max(arr[0], 0);
         for (int i = 1; i < arr.length; i++) {
